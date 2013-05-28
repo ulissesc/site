51ab8ce573c147f8076804e5f30962e0e621edb5
ulissesc <torquatro@gmail.com>
2013-05-28 11:04:33 -0300

    commit inicial do site

---
+++ app/controllers/produtos_controller.rb
@@ -0,0 +1,5 @@
+class ProdutosController < ApplicationController
+
+	def index
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Site::Application.config.secret_token = '04b1db6971443ac71c34fddefbca693ec60c0eace06dc59246e89ecf46abe78c3809a5c08dcdaffd42d6d786167df9cad4506fe16b4f1a669cd95fe95ad79c76'
